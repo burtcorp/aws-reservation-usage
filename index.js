9b37c8d9df409aa0ee2d16e6a7abf6c3df6cc8f5
Theo <theo@iconara.net>
2018-01-08 15:51:54 +0100

    Add ReservationUsage as the main entrypoint
    Rewrite index.js to be a Lambda handler and accept API Gateway requests

--- index.js
+++ index.js
@@ -1,81 +1,10 @@
-const AWS = require('aws-sdk')
-const EC2 = require('./lib/ec2')
-const ReservationManager = require('./lib/reservation_manager')
+const ReservationUsage = require('./lib/reservation_usage')
 
-const ec2 = new EC2(new AWS.EC2({region: 'eu-west-1'}))
+const reservationUsage = new ReservationUsage()
 
-function summarizeByFamily(objs, property) {
-  const summary = {}
-  objs.forEach((instance) => {
-    summary[instance.family] = (summary[instance.family] || 0) + instance[property]
-  })
-  return summary
+exports.handler = (event, context, callback) => {
+  reservationUsage
+    .processEvent(event)
+    .then(response => callback(null, response))
+    .catch(callback)
 }
-
-function sortedKeys(...objs) {
-  let keys = objs.reduce((keys, o) => keys.concat(Object.keys(o)), [])
-  keys = new Set(keys)
-  keys = [...keys].sort()
-  return keys
-}
-
-function summarizeFindings(reservations, instances, unreservedInstances, surplusReservations) {
-  const summaries = {
-    running: summarizeByFamily(instances, 'units'),
-    reserved: summarizeByFamily(reservations, 'units'),
-    unreserved: summarizeByFamily(unreservedInstances, 'units'),
-    surplus: summarizeByFamily(surplusReservations, 'remainingUnits'),
-  }
-  const families = sortedKeys(summaries.running, summaries.reserved)
-  return families.map((family) => {
-    const row = {family}
-    for (let column in summaries) {
-      row[column] = summaries[column][family] || 0
-    }
-    return row
-  })
-}
-
-function matchReservations(reservations, instances) {
-  const reservationManager = new ReservationManager(reservations)
-  const unreservedInstances = []
-  instances.forEach((instance) => {
-    const reservation = reservationManager.consumeReservedCapacity(instance)
-    if (reservation == null) {
-      unreservedInstances.push(instance)
-      console.log(JSON.stringify(instance))
-    } else {
-      console.log(JSON.stringify(instance), '->', JSON.stringify(reservation))
-    }
-  })
-  reservationManager.unusedReservedCapacity().forEach((reservation) => {
-    console.log('->', JSON.stringify(reservation))
-  })
-  return summarizeFindings(reservations, instances, unreservedInstances, reservationManager.unusedReservedCapacity())
-}
-
-function leftPad(str, pad, n) {
-  let padding = ''
-  for (let i = 0; i < Math.max(0, n - str.length); i++) {
-    padding += pad
-  }
-  return padding + str
-}
-
-function report(summary) {
-  console.log('       running   reserved unreserved    surplus')
-  for (let row of summary) {
-    let line = row.family + ' '
-    line += ' ' + leftPad(row.running.toString(), ' ', 10)
-    line += ' ' + leftPad(row.reserved.toString(), ' ', 10)
-    line += ' ' + leftPad(row.unreserved.toString(), ' ', 10)
-    line += ' ' + leftPad(row.surplus.toString(), ' ', 10)
-    console.log(line)
-  }
-}
-
-Promise
-  .all([ec2.loadReservations(), ec2.loadInstances()])
-  .then(([reservations, instances]) => matchReservations(reservations, instances))
-  .then(summary => report(summary))
-  .catch(e => console.error(e))
---
+++ test/reservation_usage_test.js
@@ -0,0 +1,148 @@
+const {describe, it, beforeEach} = require('mocha')
+const {expect} = require('chai')
+const ReservationUsage = require('../lib/reservation_usage')
+
+require('mocha-define') /* global def */
+
+describe('ReservationUsage', function () {
+  def('reservationUsage', function () {
+    return new ReservationUsage(this.env, this.ec2)
+  })
+
+  beforeEach(function () {
+    this.env = {
+      AWS_DEFAULT_REGION: 'eu-north-3',
+    }
+  })
+
+  beforeEach(function () {
+    this.ec2 = {
+      reservations: [],
+      instances: [],
+      loadReservations(region) {
+        this.requestedReservationRegion = region
+        return Promise.resolve(this.reservations)
+      },
+      loadInstances(region) {
+        this.requestedInstancesRegion = region
+        return Promise.resolve(this.instances)
+      },
+    }
+  })
+
+  beforeEach(function () {
+    this.event = {}
+  })
+
+  describe('#processEvent', function () {
+    beforeEach(function () {
+      this.ec2.instances.push({family: 'i9', size: 'large', units: 4})
+      this.ec2.instances.push({family: 'p7', size: 'large', units: 4})
+      this.ec2.instances.push({family: 'd5', size: 'large', units: 4})
+      this.ec2.reservations.push({family: 'p7', size: 'small', offeringClass: 'convertible', units: 8})
+      this.ec2.reservations.push({family: 'i9', size: 'small', offeringClass: 'convertible', units: 18})
+      this.ec2.reservations.push({family: 'i9', size: 'small', offeringClass: 'convertible', units: 4})
+      this.ec2.reservations.push({family: 'i9', size: 'small', offeringClass: 'convertible', units: 2})
+    })
+
+    describe('when the event is an API Gateway event', function () {
+      beforeEach(function () {
+        this.event = {
+          queryStringParameters: {
+            region: 'eu-north-9',
+          },
+        }
+      })
+
+      it('returns an API Gateway-compatible response', function () {
+        return this.reservationUsage.processEvent(this.event).then((response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.body).to.not.be.undefined
+          expect(response.headers).to.not.be.undefined
+        })
+      })
+
+      it('returns a plain text summary', function () {
+        return this.reservationUsage.processEvent(this.event).then((response) => {
+          expect(response.statusCode).to.equal(200)
+          expect(response.body).to.match(/running\s+reserved\s+unreserved\s+surplus/)
+          expect(response.headers['Content-Type']).to.equal('text/plain; charset=UTF-8')
+        })
+      })
+
+      it('loads instances and reservations for the region specified in the "region" parameter', function () {
+        return this.reservationUsage.processEvent(this.event).then(() => {
+          expect(this.ec2.requestedReservationRegion).to.equal('eu-north-9')
+          expect(this.ec2.requestedInstancesRegion).to.equal('eu-north-9')
+        })
+      })
+
+      describe('when no "region" parameter is specified', function () {
+        beforeEach(function () {
+          delete this.event.queryStringParameters.region
+        })
+
+        it('loads instances and reservations for the region specified in the AWS_DEFAULT_REGION environment variable', function () {
+          return this.reservationUsage.processEvent(this.event).then(() => {
+            expect(this.ec2.requestedReservationRegion).to.equal('eu-north-3')
+            expect(this.ec2.requestedInstancesRegion).to.equal('eu-north-3')
+          })
+        })
+      })
+    })
+
+    describe('when the event any non-API Gateway-event', function () {
+      describe('returns a summary that', function () {
+        it('contains the families of the instances and reservations, in alphabetical order', function () {
+          return this.reservationUsage.processEvent(this.event).then((response) => {
+            expect(response.map(s => s.family)).to.deep.equal([
+              'd5',
+              'i9',
+              'p7',
+            ])
+          })
+        })
+
+        it('contains the the number of running units for each instance family', function () {
+          return this.reservationUsage.processEvent(this.event).then((response) => {
+            expect(response.map(s => [s.family, s.running])).to.deep.equal([
+              ['d5', 4],
+              ['i9', 4],
+              ['p7', 4],
+            ])
+          })
+        })
+
+        it('contains the the number of reserved units for each instance family', function () {
+          return this.reservationUsage.processEvent(this.event).then((response) => {
+            expect(response.map(s => [s.family, s.reserved])).to.deep.equal([
+              ['d5', 0],
+              ['i9', 18 + 4 + 2],
+              ['p7', 8],
+            ])
+          })
+        })
+
+        it('contains the the number of unreserved units for each instance family', function () {
+          return this.reservationUsage.processEvent(this.event).then((response) => {
+            expect(response.map(s => [s.family, s.unreserved])).to.deep.equal([
+              ['d5', 4],
+              ['i9', 0],
+              ['p7', 0],
+            ])
+          })
+        })
+
+        it('contains the the number of unused reserved units for each instance family', function () {
+          return this.reservationUsage.processEvent(this.event).then((response) => {
+            expect(response.map(s => [s.family, s.surplus])).to.deep.equal([
+              ['d5', 0],
+              ['i9', 18 + 4 + 2 - 4],
+              ['p7', 8 - 4],
+            ])
+          })
+        })
+      })
+    })
+  })
+})
